743a4182d3be9b6f85947cb9b21d483ef409d00e
Uttarayan <uttarayan21@gmail.com>
2023-11-27 01:59:21 +0530

    [feat] Add my own keymaps

---
+++ keyboards/keychron/k6/rgb/ansi/keymaps/servius/keymap.c
@@ -0,0 +1,118 @@
+/*
+Copyright 2022 Dimitris Mantzouranis <d3xter93@gmail.com>
+Copyright 2022 Pablo Ramirez
+Copyright 2023 Santanu Paik (https://github.com/ITNerd69)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include QMK_KEYBOARD_H
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+enum layer_names {
+    WIN = 0,
+    MAC = 1,
+    FN1 = 2,
+    FN2 = 3,
+    /* FN3 = 4, */
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Windows Base
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
+     * │Esc│ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │ Backsp│MOD│
+     * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┼───┤
+     * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  \  │HOM│
+     * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┤
+     * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │  Enter │PGU│
+     * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
+     * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │ Shift│ ↑ │PGD│
+     * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
+     * │Ctrl│GUI │Alt │         Space          │Ctl│Fn1│Fn2│ ← │ ↓ │ → │
+     * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
+     */
+    [WIN] = LAYOUT_ansi(
+        QK_GESC, KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,     KC_MINS,     KC_EQL,  KC_BSPC,            KC_DEL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,     KC_LBRC,     KC_RBRC, KC_BSLS,            KC_HOME,
+        KC_CAPS, KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H,   KC_J, KC_K, KC_L,    KC_SCLN,  KC_QUOT,              KC_ENT,             KC_PGUP,
+        KC_LSFT,          KC_Z,    KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,   KC_SLSH,              KC_RSFT,   KC_UP,   KC_PGDN,
+        MO(FN1), KC_LGUI, KC_LALT,                   KC_SPC,                      KC_RCTL,  MO(FN1),     MO(FN2), KC_LEFT,   KC_DOWN, KC_RGHT
+    ),
+    /* Mac Base
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
+     * │Esc│ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │ Backsp│MOD│
+     * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┼───┤
+     * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  \  │HOM│
+     * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┤
+     * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │  Enter │PGU│
+     * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
+     * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │ Shift│ ↑ │PGD│
+     * ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
+     * │Ctrl│Alt │GUI │         Space          │GUI│Fn1│Fn2│ ← │ ↓ │ → │
+     * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
+     */
+    [MAC] = LAYOUT_ansi(
+        QK_GESC, KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,    KC_MINS,     KC_EQL,  KC_BSPC,            KC_DEL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,    KC_LBRC,     KC_RBRC, KC_BSLS,            KC_HOME,
+        KC_LCTL, KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H,   KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT,              KC_ENT,             KC_PGUP,
+        KC_LSFT,          KC_Z,    KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH,              KC_RSFT,   KC_UP,   KC_PGDN,
+        MO(FN1), KC_LALT, KC_LGUI,                   KC_SPC,                      KC_RGUI, MO(FN1),     MO(FN2), KC_LEFT,   KC_DOWN, KC_RGHT
+    ),
+    [FN1] = LAYOUT_ansi(
+        KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  _______,         RGB_MOD,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  KC_INS,  KC_DEL,  KC_END,  _______,          _______,
+        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,          _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,          _______, _______, _______,
+        _______, _______, _______,                            _______,                             KC_RALT, _______, _______, _______, _______, _______
+    ),
+    [FN2] = LAYOUT_ansi(
+        KC_GRV,  KC_BRID, KC_BRIU, _______, _______,  RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, QK_BOOT,          RGB_TOG,
+        _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,          _______,          RGB_SPI,
+        _______,          _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,          _______, RGB_SAI, RGB_SPD,
+        _______, _______, _______,                             _______,                            _______, _______, _______, RGB_HUD, RGB_SAD, RGB_HUI
+    ),
+    /* [FN3] = LAYOUT_ansi( */
+    /*     KC_GRV,  KC_BRID, KC_BRIU, _______, _______,  RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, Q_RESET,          RGB_TOG, */
+    /*     _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, */
+    /*     _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,          _______,          RGB_SPI, */
+    /*     _______,          _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,          _______, RGB_SAI, RGB_SPD, */
+    /*     _______, _______, _______,                             _______,                            _______, _______, _______, RGB_HUD, RGB_SAD, RGB_HUI */
+    /* ) */
+};
+
+
+bool dip_switch_update_user(uint8_t index, bool active) {
+  switch(index) {
+    case 0: // OS switch
+      if (active) { // Mac/iOS mode
+        layer_move(WIN);
+      }
+      else { // Windows/Android mode
+        layer_move(MAC);
+      }
+      break;
+  }
+  return true;
+}
+
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  // debug_enable = true;
+  // debug_matrix = true;
+  // debug_keyboard = true;
+  // debug_mouse = true;
+}
