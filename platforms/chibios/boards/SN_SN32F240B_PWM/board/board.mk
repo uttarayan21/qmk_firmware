3ca51778de2f680b1445697916f1f1e858c3baba
Dimitris Mantzouranis <d3xter93@gmail.com>
2024-09-11 16:44:20 +0300

    sn32: boards: pwm: disable SWD on init

--- platforms/chibios/boards/SN_SN32F240B_PWM/board/board.mk
+++ platforms/chibios/boards/SN_SN32F240B_PWM/board/board.mk
@@ -1,6 +1,9 @@
 # List of all the board related files.
 BOARDSRC = $(CHIBIOS_CONTRIB)/os/hal/boards/SN_SN32F240B/board.c
 
+# Extra files
+BOARDSRC += $(BOARD_PATH)/board/extra.c
+
 # Required include directories
 BOARDINC = $(CHIBIOS_CONTRIB)/os/hal/boards/SN_SN32F240B
 
